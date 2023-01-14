2082abcd27c666ac60e0681c2a7569a520769b49
Premforreal <93695810+Premforreal@users.noreply.github.com>
2023-01-14 16:00:43 +0530

    search functionality done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+  const [search, setSearch] = useState("");
+
+  axios.get("https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json")
+      .then((res)=>setData(res.data))
+      .catch((err)=>console.log(err))
+  
+  function deleteItem(id) {
+    console.log(id);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <div className='search'>
+            <input  placeholder='search by name, email or role' 
+                    type="text" 
+                    onChange={(e)=>setSearch(e.target.value)}/>
+        </div>
+        
+        <tbody>
+          <tr>
+            <th>
+              <input type="checkbox" />
+            </th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Actions</th>
+          </tr>
+          {/* https://www.freecodecamp.org/news/how-the-question-mark-works-in-javascript/ */}
+          
+          {data?.filter((item)=>{
+                return search.toLowerCase()==="" ? item :
+                    ( item.name.toLowerCase().includes(search)  || 
+                      item.email.toLowerCase().includes(search) || 
+                      item.role.toLowerCase().includes(search)
+                    ) 
+            }).map((item) => (
+              <tr key={item.id}>
+                <th>
+                  <input type="checkbox" />
+                </th>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.role}</td>
+                <td>
+                  <button>
+                    edit
+                  </button>
+                  <button onClick={()=>deleteItem(item.id)}>
+                    delete
+                  </button>
+                </td>
+              </tr>
+           ))}
+        </tbody>
+    </>
   );
 }
 
